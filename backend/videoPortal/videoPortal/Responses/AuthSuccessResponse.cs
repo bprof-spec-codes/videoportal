c66fb2d61ed2d2d9e9e917bd41f2ea0bbf607b6e
szappanosbenedek@stud.uni-obuda.hu <szappanosbenedek@stud.uni-obuda.hu>
2023-05-02 14:17:40 +0200

    responses added

---
+++ backend/videoPortal/videoPortal/Responses/AuthSuccessResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace videoPortal.Responses
+{
+    public class AuthSuccessResponse
+    {
+        public string Token { get; set; }
+
+        public string RefreshToken { get; set; }
+    }
+}
