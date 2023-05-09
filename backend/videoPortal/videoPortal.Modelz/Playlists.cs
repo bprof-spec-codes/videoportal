f2b307a585c6376d8ab879443d02d0a297146ba2
czeglediaron <czeglediaron@gmail.com>
2023-05-09 23:12:41 +0200

    repository init

---
+++ backend/videoPortal/videoPortal.Modelz/Playlists.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videoPortal.Modelz
+{
+    public class Playlists
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public virtual ICollection<Playlist> Playlist { get; set; }
+
+        public Playlists(HashSet<Playlist> Playlist)
+        {
+            Id = Guid.NewGuid().ToString();
+            this.Playlist = Playlist;
+        }
+    }
+}
---
+++ backend/videoPortal/videoPortal.Repository/PlaylistsRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using videoPortal.Data;
+using videoPortal.Modelz;
+
+namespace videoPortal.Repository
+{
+    public class PlaylistsRepository
+    {
+        videoPortalDbContext context;
+
+        public PlaylistsRepository(videoPortalDbContext context)
+        {
+            this.context = context;
+        }
+
+      
+
+    }
+}
