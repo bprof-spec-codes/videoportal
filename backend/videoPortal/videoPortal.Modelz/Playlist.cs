a6326a223b6f4637542e381668e963d7c5d49421
czeglediaron <czeglediaron@gmail.com>
2023-05-09 23:12:21 +0200

    playlist repo

---
+++ backend/videoPortal/videoPortal.Modelz/Playlist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace videoPortal.Modelz
+{
+    public class Playlist
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Creator { get; set; }
+        [Required]
+        public string Playtime { get; set; }
+
+        public Playlist(string Title, string Creator, string Playtime)
+        {
+            Id = Guid.NewGuid().ToString();
+            this.Title = Title;
+            this.Creator = Creator;
+            this.Playtime = Playtime;
+        }
+
+    }
+}
---
+++ backend/videoPortal/videoPortal.Repository/PlaylistRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using videoPortal.Data;
+using videoPortal.Modelz;
+
+namespace videoPortal.Repository
+{
+    public class PlaylistRepository
+    {
+        videoPortalDbContext context;
+
+        public PlaylistRepository(videoPortalDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(Playlist playlist)
+        {
+            context.playlist.Add(playlist);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<Playlist> Read()
+        {
+            return context.playlist;
+        }
+
+        public Playlist? Read(string id)
+        {
+            return context.playlist.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Delete(string id)
+        {
+            var playlist = Read(id);
+            context.playlist.Remove(playlist);
+            context.SaveChanges();
+        }
+    }
+}
