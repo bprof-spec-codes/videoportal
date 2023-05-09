9160965756fed48e244fb3f5059efc1f4cdd79c1
czeglediaron <czeglediaron@gmail.com>
2023-05-09 23:10:54 +0200

    dbset created

--- backend/videoPortal/videoPortal.Data/videoPortalDbContext.cs
+++ backend/videoPortal/videoPortal.Data/videoPortalDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Data.Entity;
+using videoPortal.Modelz;
 
 namespace videoPortal.Data
 {
     public class videoPortalDbContext: DbContext
     {
+        public DbSet<Playlists> playlists { get; set; }
+        public DbSet<Playlist> playlist { get; set; }
+
         public videoPortalDbContext()
         {
 
