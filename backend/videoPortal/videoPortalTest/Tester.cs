9f0e05a3dbfb1f7ebb24d57bd8e088135d687c46
czeglediaron <czeglediaron@gmail.com>
2023-05-09 23:13:17 +0200

    no actual changes

--- backend/videoPortal/videoPortalTest/Tester.cs
+++ backend/videoPortal/videoPortalTest/Tester.cs
@@ -1,6 +1,12 @@
-﻿using NUnit.Framework;
+﻿using IdentityServer3.Core.Services;
+using Microsoft.AspNetCore.Authentication;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Data.Entity;
+using videoPortal;
+using videoPortal.Controllers;
+using Xunit;
 
 namespace videoPortalTest
 {
@@ -15,8 +21,14 @@ namespace videoPortalTest
             var v1 = "Hello World";
             Assert.That(v1, Is.EqualTo("Hello World"));
         }
-       
 
 
+        [Fact]
+        public void ShouldAuthenticateValidUser()
+        {
+            var mockService = new Mock<IConfig>();
+            UserDto u = new UserDto() { UserName = "Username", Password = "Password" };
+            mockService.Setup(x=> x.Logom())  
+        }
     }
 }
