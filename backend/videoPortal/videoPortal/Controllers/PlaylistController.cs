f3f37185660a7b89712e7f9e8cff1f9246dfcd02
czeglediaron <czeglediaron@gmail.com>
2023-05-11 22:26:51 +0200

    controllers created

---
+++ backend/videoPortal/videoPortal/Controllers/PlaylistController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using videoPortal.Modelz;
+using videoPortal.Repository;
+
+namespace videoPortal.Controllers
+{
+    public class PlaylistController : Controller
+    {
+        IPlaylistRepository repo;
+
+        public PlaylistController(IPlaylistRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreatePlaylist(Playlist playlist)
+        {
+            this.repo.Create(playlist);
+            return RedirectToAction(nameof(Index));
+        }
+        
+        
+        public IActionResult ReadPlaylist()
+        {
+            this.repo.Read();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult ReadPlaylist(string id)
+        {
+            this.repo.Read(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+     
+        public IActionResult DeletePlaylist(string id)
+        {
+            this.repo.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ backend/videoPortal/videoPortal/Controllers/PlaylistsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using videoPortal.Modelz;
+using videoPortal.Repository;
+
+namespace videoPortal.Controllers
+{
+    public class PlaylistsController : Controller
+    {
+        IPlaylistsRepository repo;
+
+        public PlaylistsController(IPlaylistsRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreatePlaylists(Playlists playlists)
+        {
+            this.repo.Create(playlists);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult ReadPlaylists()
+        {
+            this.repo.Read();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult ReadPlaylists(string id)
+        {
+            this.repo.Read(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+        public IActionResult DeletePlaylists(string id)
+        {
+            this.repo.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
