cce5271bf084d8cc872dc776cfd767ca29e4163e
czeglediaron <czeglediaron@gmail.com>
2023-05-28 16:56:20 +0200

    playlist added

---
+++ backend/videoPortal/videoPortal/Models/Playlist.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace videoPortal.Models
+{
+    public class Playlist
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        [NotMapped]
+        public IdentityUser Creator { get; set; }
+        public string Playtime { get; set; }
+    }
+}
